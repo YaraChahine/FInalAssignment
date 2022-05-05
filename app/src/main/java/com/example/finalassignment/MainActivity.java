01f9ebdfcb9dd9c2166fe1e43d67bb39827846e9
Yara Chahine <79076389+YaraChahine@users.noreply.github.com>
2022-05-05 20:19:49 +0300

    created list view

--- app/src/main/java/com/example/finalassignment/MainActivity.java
+++ app/src/main/java/com/example/finalassignment/MainActivity.java
@@ -5,16 +5,23 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.ArrayList;
 
+public class MainActivity extends AppCompatActivity {
+    ListView listview;
+    ArrayList<String> courses_array;
+    ArrayAdapter<String> adapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
+        listview= (ListView) findViewById(R.id.coursesList);
+        courses_array =new ArrayList<String>();
         try{
 
             SQLiteDatabase sql= this.openOrCreateDatabase("assignmentdb",MODE_PRIVATE,null);
@@ -30,9 +37,13 @@ public class MainActivity extends AppCompatActivity {
             int examCourseIndex = c.getColumnIndex("exam_course");
             c.moveToFirst();
             while (c!=null){
+                courses_array.add("test");
                 Toast.makeText(getApplicationContext(),c.getString(examCourseIndex),Toast.LENGTH_LONG).show();
                 c.moveToNext() ;
             }
+            adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1,courses_array);
+
+            listview.setAdapter(adapter);
 
         }
         catch(Exception e) {
