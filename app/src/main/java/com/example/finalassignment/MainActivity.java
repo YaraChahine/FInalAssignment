65f4adf64a56664acfc407f086ba9ace600d5e80
Yara Chahine <79076389+YaraChahine@users.noreply.github.com>
2022-05-05 19:57:17 +0300

    started with mysqlite

--- app/src/main/java/com/example/finalassignment/MainActivity.java
+++ app/src/main/java/com/example/finalassignment/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.finalassignment;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,30 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        try{
+
+            SQLiteDatabase sql= this.openOrCreateDatabase("assignmentdb",MODE_PRIVATE,null);
+            sql.execSQL("CREATE Table IF NOT EXISTS exams (exam_course VARCHAR)");
+//            sql.execSQL(("INSERT INTO exams(exam_course) VALUES ('Discrete Structures II')"));
+//            sql.execSQL(("INSERT INTO exams(exam_course) VALUES ('Parallel Programming')"));
+//            sql.execSQL(("INSERT INTO exams(exam_course) VALUES ('Game Development')"));
+//            sql.execSQL(("INSERT INTO exams(exam_course) VALUES ('Mobile Develpoment')"));
+//            sql.execSQL(("INSERT INTO exams(exam_course) VALUES ('Capstone')"));
+
+
+            Cursor c=sql.rawQuery("Select * from exams", null);
+            int examCourseIndex = c.getColumnIndex("exam_course");
+            c.moveToFirst();
+            while (c!=null){
+                Toast.makeText(getApplicationContext(),c.getString(examCourseIndex),Toast.LENGTH_LONG).show();
+                c.moveToNext() ;
+            }
+
+        }
+        catch(Exception e) {
+            e.printStackTrace();
+        }
     }
 }
